00b3b1864c2822728e1970b7913a20675db0a8e9
Sevann Triztan <striztan@dacgroup.com>
2022-07-21 07:20:16 -0300

    Create and Download static excel file

---
+++ CreateDownloadExcelFile.Api/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+using ClosedXML.Excel;
+using CreateDownloadExcelFile.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreateDownloadExcelFile.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly List<Student> _students = new();
+
+        public HomeController()
+        {
+            for (int i = 0; i < 21; i++)
+            {
+                _students.Add(new()
+                {
+                    Id = i,
+                    Name = $"Studend Name: {i}",
+                    Roll = $"100{i}"
+                });
+            }
+        }
+
+        [HttpGet(Name = "CreateAndDownloadFile")]
+        public IActionResult CreateAndDownloadFile()
+        {
+            using (XLWorkbook? workbook = new())
+            {
+                IXLWorksheet? worksheet = workbook.Worksheets.Add("Students");
+                int currentRow = 1;
+
+                #region Header
+                worksheet.Cell(currentRow, 1).Value = "StudentId";
+                worksheet.Cell(currentRow, 2).Value = "Name";
+                worksheet.Cell(currentRow, 3).Value = "Roll";
+                #endregion
+
+                #region Body
+                _ = _students
+                    .Select(s =>
+                    {
+                        currentRow++;
+                        worksheet.Cell(currentRow, 1).Value = s.Id;
+                        worksheet.Cell(currentRow, 2).Value = s.Name;
+                        worksheet.Cell(currentRow, 3).Value = s.Roll;
+
+                        return s;
+                    })
+                    .ToList();
+                #endregion
+
+                using MemoryStream? stream = new();
+                workbook.SaveAs(stream);
+                byte[]? content = stream.ToArray();
+
+                return File(
+                    content,
+                    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    $"Students {DateTime.UtcNow}.xlsx");
+            }
+        }
+    }
+}
---
+++ CreateDownloadExcelFile.Api/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace CreateDownloadExcelFile.Api.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Roll { get; set; } = string.Empty;
+    }
+}
